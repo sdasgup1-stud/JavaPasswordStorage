367f72ef7e69888ec2758e7d57aa44da07244dee
AdSHSO <44525339+AdSHSO@users.noreply.github.com>
2018-11-15 18:34:24 +0100

    added
    Signed-off-by: AdSHSO <44525339+AdSHSO@users.noreply.github.com>

--- Password.java
+++ Password.java
@@ -7,6 +7,7 @@ import javax.crypto.spec.PBEKeySpec;
 import java.security.SecureRandom;
 import org.apache.commons.codec.binary.Base64;
 
+
 public class Password {
     // The higher the number of iterations the more 
     // expensive computing the hash is for us and
